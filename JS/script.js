a175d103b7af2d64bc691f04a145f22cf2922759
Brayan pereira <bp410794@gmail.com>
2024-03-12 13:45:57 -0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+//Menu hambuger
+
+const btnOn = document.querySelector("#btn_menu")
+const menu = document.querySelector(".menu_hamburger")
+const btnOff = document.querySelector("#btnOff_menu")
+
+btnOn.addEventListener("click", () =>{
+        menu.style.display = "block"
+        
+})
+
+btnOff.addEventListener("click", () =>{
+    menu.style.display= "none"
+} )
+
+
+
